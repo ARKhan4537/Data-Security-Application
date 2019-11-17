0a8acda8827c29207dc55e4e6038438b6421a031
AR Khan <39606049+ARKhan4537@users.noreply.github.com>
2019-11-17 01:42:42 -0800

    Add files via upload

---
+++ Data Security.cpp
@@ -0,0 +1,365 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <conio.h>
+#include <iomanip>
+#include <fstream>
+//Preparing Stucher Design of a software
+using namespace std;
+
+class Lockfiles_USB 
+{
+	private:
+		
+	int a,b,c;
+    
+    public:
+
+      void  ()
+	  {
+      	
+      	
+	  }
+      
+};
+
+
+class PasswordAuthentication_USB 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+
+class UnableFormatData_USB 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+class MonitoringActivity_USB 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+class FireWall_USB
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+class LockspecificFolder_Drive 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+class PasswordAuthentication_Drive 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+class HideFolderChangeIcon_Drive 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+class MonitoringActivity_Drive 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+class Firewall_Drive 
+{
+	private:
+	char firstName[50];
+	char SecondName[50];
+	int ID;
+    
+    public:
+      void takeName()
+      {
+       cout<<"Enter your First Name:";
+       cin>>firstName;
+       cout<<"Enter your Second Name:";
+       cin>>SecondName; 
+       }  	
+       
+       void takeIDNumber()
+       {
+        cout<<"Enter your Id Number:";
+        cin>>ID;      
+       }
+       
+       void output()
+       {
+        cout<<"your First Name is:" <<firstName<<endl;
+        cout<<"your Second Name is:"<<SecondName<<endl;
+        cout<<"your  value is"<<ID;     
+        system("pause");    
+       }
+};
+
+int main()
+{
+
+
+  //Interface Design Here...
+
+
+int Optionchoice;
+cout<<"";
+cout<<"";
+cout<<"=========================================";
+cout<<""<<endl;
+cout<<"Press 1 For Install Software in  Usb";
+cout<<""<<endl;
+cout<<""<<endl;
+cout<<""<<endl;
+cout<<"Press 2 For Install Software in  Drive";
+cout<<""<<endl;
+cout<<"=========================================";
+cout<<"";
+cout<<"";
+cout<<"";
+cout<<"";
+cout<<"";
+cout<<"";
+cout<<"";
+cout<<"Choose your Option:";
+cin>>Optionchoice;
+
+
+
+   //start   Usb        Classes
+
+
+Lockfiles_Usb                   //Class
+PasswordAuthentication_USB      //Class
+UnableFormatData_USB            //Class
+MonitoringActivity_USB          //Class
+FireWall_USB                    //Class
+
+
+  //Usb    Classes     Stop
+
+
+
+// Drive  Classes    Start
+
+LockspecificFolder_Drive        //Class
+PasswordAuthentication_Drive    //Class
+HideFolderChangeIcon_Drive      //Class
+MonitoringActivity_Drive        //Class
+FireWall_Drive                  //Class
+
+//Drive  Classes     Stop
+
+
+
+return 0;
+}
